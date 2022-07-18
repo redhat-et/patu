ccf6d3483b88fbdfaa91b24a2785b55fc167d82d
Punakshi <punakshi.chaand.pc@gmail.com>
2022-07-18 14:06:56 -0700

    #50 change

--- bpf/include/helpers/helpers.h
+++ bpf/include/helpers/helpers.h
@@ -72,6 +72,6 @@ static __u64 BPF_FUNC(get_current_pid_tgid);
 static __u64 BPF_FUNC(get_current_uid_gid);
 static void BPF_FUNC(trace_printk, const char *fmt, int fmt_size, ...);
 static int BPF_FUNC(sock_hash_update, struct bpf_sock_ops *skops,
-                    struct bpf_map *map, void *key, __u64 flags);
+                    struct btf_map *map, void *key, __u64 flags);
 static long BPF_FUNC(sk_redirect_hash, struct __sk_buff *skb,
-                     struct bpf_map *map, void *key, __u64 flag);
+                     struct btf_map *map, void *key, __u64 flag);
