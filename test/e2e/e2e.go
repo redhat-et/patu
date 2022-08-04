f53a49076ce4391a9aea18cd1fa43738fa841caa
Brent Salisbury <bsalisbu@redhat.com>
2022-08-04 15:47:45 -0400

    Added ginkgo CI e2e framework
    Added gingko and an initial test for patterning (#81)
    
    Signed-off-by: Brent Salisbury <bsalisbu@redhat.com>

---
+++ test/e2e/e2e.go
@@ -0,0 +1,80 @@
+/*
+ * Copyright © 2022 Authors of Patu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package e2e
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/onsi/ginkgo"
+	"k8s.io/kubernetes/test/e2e/framework"
+)
+
+const (
+	patuSockOps        = "patu_sockops"
+	patuSkMsg          = "patu_skmsg"
+	patuCgroupSockSend = "patu_sendmsg4"
+	patuCgroupSockRecv = "patu_recvmsg4"
+)
+
+// runCommand runs the cmd and returns the combined stdout and stderr
+func runCommand(cmd ...string) (string, error) {
+	output, err := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("failed to run %q: %s (%s)", strings.Join(cmd, " "), err, output)
+	}
+	return string(output), nil
+}
+
+// This test validates that the patu bpf programs are loaded
+/* This test does the following:
+   1. Verify sock_ops named patu_sockops program is loaded
+   2. Verify sk_msg program named patu_skmsg is loaded
+   3. Verify cgroup_sock_addr program named patu_sendmsg4 is loaded
+   4. Verify cgroup_sock_addr program named patu_recvmsg4 is loaded
+*/
+var _ = ginkgo.Describe("Validate patu ebpf programs are loaded", func() {
+	var bpfOut string
+	var err error
+	ginkgo.BeforeEach(func() {
+		bpfOut, err = runCommand("sudo", "bpftool", "prog", "list")
+		if err != nil {
+			framework.Failf("failed to run bpftool cmd: %v", err)
+		}
+	})
+	ginkgo.It("1. Verify sock_ops named patu_sockops program is loaded", func() {
+		if !strings.Contains(bpfOut, patuSockOps) {
+			framework.Failf("The program %s was not found\n", patuSockOps)
+		}
+	})
+	ginkgo.It("2. Verify sk_msg program named patu_skmsg is loaded", func() {
+		if !strings.Contains(bpfOut, patuSkMsg) {
+			framework.Failf("The program %s was not found\n", patuSkMsg)
+		}
+	})
+	ginkgo.It("3. Verify cgroup_sock_addr program named patu_sendmsg4 is loaded", func() {
+		if !strings.Contains(bpfOut, patuCgroupSockSend) {
+			framework.Failf("The program %s was not found\n", patuCgroupSockSend)
+		}
+	})
+	ginkgo.It("4. Verify cgroup_sock_addr program named patu_sendmsg4 is loaded", func() {
+		if !strings.Contains(bpfOut, patuCgroupSockRecv) {
+			framework.Failf("The program %s was not found\n", patuCgroupSockRecv)
+		}
+	})
+})
---
+++ test/e2e/go.mod
@@ -0,0 +1,43 @@
+module github.com/redhat-et/patu/test/e2e
+
+go 1.16
+
+require (
+	github.com/onsi/ginkgo v1.14.0
+	github.com/onsi/gomega v1.10.1
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	k8s.io/api v0.24.3 // indirect
+	k8s.io/kubernetes v0.0.0-00010101000000-000000000000
+)
+
+replace (
+	k8s.io/api => k8s.io/api v0.24.0
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.24.0
+	k8s.io/apimachinery => k8s.io/apimachinery v0.24.0
+	k8s.io/apiserver => k8s.io/apiserver v0.24.0
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.24.0
+	k8s.io/client-go => k8s.io/client-go v0.24.0
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.24.0
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.24.0
+	k8s.io/code-generator => k8s.io/code-generator v0.24.0
+	k8s.io/component-base => k8s.io/component-base v0.24.0
+	k8s.io/component-helpers => k8s.io/component-helpers v0.24.0
+	k8s.io/controller-manager => k8s.io/controller-manager v0.24.0
+	k8s.io/cri-api => k8s.io/cri-api v0.24.0
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.24.0
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.24.0
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.24.0
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.24.0
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.24.0
+	k8s.io/kubectl => k8s.io/kubectl v0.24.0
+	k8s.io/kubelet => k8s.io/kubelet v0.24.0
+	k8s.io/kubernetes => k8s.io/kubernetes v1.24.0
+	k8s.io/kubernetes/test/utils/image => k8s.io/kubernetes/test/utils/image v0.24.0
+	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.24.0
+	k8s.io/metrics => k8s.io/metrics v0.24.0
+	k8s.io/mount-utils => k8s.io/mount-utils v0.24.0
+	k8s.io/pod-security-admission => k8s.io/pod-security-admission v0.24.0
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.24.0
+)
