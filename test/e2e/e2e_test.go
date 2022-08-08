67b3b6b66019fd305b0bcc97e624b31b6518fe3a
Brent Salisbury <bsalisbu@redhat.com>
2022-08-08 18:55:14 -0400

    Added k8s framework flags in e2e init
    This adds the common flags needed for k8s framework tests
    
    Signed-off-by: Brent Salisbury <bsalisbu@redhat.com>

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -17,12 +17,64 @@
 package e2e
 
 import (
-	"github.com/onsi/ginkgo"
-	"github.com/onsi/gomega"
+	"flag"
+	"fmt"
+	"os"
+	"path"
 	"testing"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/config"
+	"github.com/onsi/ginkgo/reporters"
+	"github.com/onsi/gomega"
+	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/test/e2e/framework"
+	e2econfig "k8s.io/kubernetes/test/e2e/framework/config"
+	"k8s.io/kubernetes/test/e2e/framework/testfiles"
+	"k8s.io/kubernetes/test/utils/image"
 )
 
+// handleFlags sets up all flags and parses the command line.
+func handleFlags() {
+	e2econfig.CopyFlags(e2econfig.Flags, flag.CommandLine)
+	framework.RegisterCommonFlags(flag.CommandLine)
+	framework.RegisterClusterFlags(flag.CommandLine)
+	flag.Parse()
+}
+
+// required due to go1.13
+func TestMain(m *testing.M) {
+	// Register test flags, then parse flags.
+	handleFlags()
+
+	if framework.TestContext.ListImages {
+		for _, v := range image.GetImageConfigs() {
+			fmt.Println(v.GetE2EImage())
+		}
+		os.Exit(0)
+	}
+
+	framework.AfterReadingAllFlags(&framework.TestContext)
+
+	if framework.TestContext.RepoRoot != "" {
+		testfiles.AddFileSource(testfiles.RootFileSource{Root: framework.TestContext.RepoRoot})
+	}
+
+	os.Exit(m.Run())
+}
+
 func TestE2e(t *testing.T) {
-	gomega.RegisterFailHandler(ginkgo.Fail)
-	ginkgo.RunSpecs(t, "Patu E2e Suite")
+	// Run tests through the Ginkgo runner with output to console + JUnit for reporting
+	var r []ginkgo.Reporter
+	if framework.TestContext.ReportDir != "" {
+		klog.Infof("Saving reports to %s", framework.TestContext.ReportDir)
+		if err := os.MkdirAll(framework.TestContext.ReportDir, 0755); err != nil {
+			klog.Errorf("Failed creating report directory: %v", err)
+		} else {
+			r = append(r, reporters.NewJUnitReporter(path.Join(framework.TestContext.ReportDir, fmt.Sprintf("junit_%v%02d.xml", framework.TestContext.ReportPrefix, config.GinkgoConfig.ParallelNode))))
+		}
+	}
+
+	gomega.RegisterFailHandler(framework.Fail)
+	ginkgo.RunSpecsWithDefaultAndCustomReporters(t, "Patu E2e Suite", r)
 }
